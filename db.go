ede24f4a02d731c7fdd032cbf0f679e26b8ed666
25s02 <k.krishna@chuo-ai.jp>
2026-05-13 05:20:37 +0000

    ok

--- db.go
+++ db.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"database/sql"
-	"log"
 
 	_ "github.com/mattn/go-sqlite3"
+	"log"
 )
 
 var db *sql.DB
--- handler.go
+++ handler.go
@@ -4,8 +4,6 @@ import (
 	"database/sql"
 	"encoding/json"
 	"net/http"
-
-	_ "github.com/mattn/go-sqlite3"
 )
 
 type Order struct {
